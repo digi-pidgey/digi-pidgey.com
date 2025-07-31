5e5c22480e1810a04659ee151fa4580e0b5dc671
digi-pidgey <lemoinebot@gmail.com>
2025-07-31 02:22:33 -0500

    Update digi-pidgey.py

--- digi-pidgey.py
+++ digi-pidgey.py
@@ -48,7 +48,7 @@ section.stMain .block-container {
 
 # javascript player
 
-<iframe src="//digi-pidgeycom-cdnhzdrjmutfuwug5ktk7u.streamlit.app/player/playerjs.html?file=//plrjs.com/x.mp4" type="text/html" width="640" height="360" frameborder="0" allowfullscreen></iframe>
+st.markdown('<iframe src="//digi-pidgeycom-cdnhzdrjmutfuwug5ktk7u.streamlit.app/player/playerjs.html?file=//plrjs.com/x.mp4" type="text/html" width="640" height="360" frameborder="0" allowfullscreen></iframe>', unsafe_allow_html=True)
 
 # command to create text / line spaces in streamlit
 st.write("")
